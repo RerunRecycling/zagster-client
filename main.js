06f0f8d3074b52e6da55199da5ed5ebc87fcac97
Quentin <q.gooding@hotmail.com>
2019-11-18 15:44:25 -0800

    Milestone Project 1
    changed javascript

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
 $(updateView)
 
 function updateView() {
-    alert("Quentin's exercise 14")
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
 }
